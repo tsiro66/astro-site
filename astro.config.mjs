6fdd63f27a9b42d920f8c2c62d57411c97fd5279
Thodoris Tsironis <theogr99@gmail.com>
2026-02-26 01:34:48 +0200

    Performance changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,14 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   site: 'https://www.flux-web.com/',
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    build: {
+      assetsInlineLimit: 10240, // Inlines assets smaller than 10kb
+    }
   },
   output:'server',
   adapter: vercel(),
   build: {
     inlineStylesheets: 'always'
-  }
+  },
 });
