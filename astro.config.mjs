97f174f593f5af49c3e8422bc86935b09adaf728
Thodoris Tsironis <theogr99@gmail.com>
2026-02-25 18:00:54 +0200

    SEO and accessability changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.flux-web.com/',
   vite: {
     plugins: [tailwindcss()]
   },
