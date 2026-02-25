150319ad4eeaed564197af353590ac8709d01622
Thodoris Tsironis <theogr99@gmail.com>
2026-02-26 01:28:30 +0200

    Performance changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,5 +12,8 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
   output:'server',
-  adapter: vercel()
+  adapter: vercel(),
+  build: {
+    inlineStylesheets: 'always'
+  }
 });
