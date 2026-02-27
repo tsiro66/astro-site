bdaeeb7b23b4de1756a9fc52929eefeb7f42aa06
Thodoris Tsironis <theogr99@gmail.com>
2026-02-27 02:26:50 +0200

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,22 @@ import tailwindcss from '@tailwindcss/vite';
 
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.flux-web.com/',
+  site: 'https://www.flux-web.com',
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   output:'server',
   adapter: vercel(),
+
   build: {
     inlineStylesheets: 'always'
   },
+
+  integrations: [sitemap()],
 });
